0b4c9d8512eb70c6d3e526875130211abc13af77
JulienAfromRTE <261381415+JulienAfromRTE@users.noreply.github.com>
2026-05-20 18:46:58 +0200

    v1.4

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 APP_NAME    = "Parentix"
 APP_SLUG    = "parentix"
-APP_RELEASE = "v1.3"
+APP_RELEASE = "v1.4"
 APP_DESCRIPTION = "Pilotage de l'association FCPE : taches, depenses, recettes, evenements, kermesse"
 APP_ICON    = "🏫"
 APP_COLOR   = "#1e40af"
